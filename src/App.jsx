36476a5f7e0486ad2dc3cf1daa34d3db0b93d48f
Ray Lin <raylin009@gmail.com>
2019-09-23 15:25:15 -0700

    changed Header.jsx to Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Layout/Header.jsx';
+import Nav from './Layout/Nav.jsx';
 import Footer from './Layout/Footer.jsx';
 import Landing from './Components/Landing.jsx';
 import AboutMe from './Components/AboutMe.jsx';
@@ -9,7 +9,7 @@ import style from './style.css';
 
 const App = () => (
   <div className={`${style.wraper}`}>
-    <Header />
+    <Nav />
     <Landing />
     <AboutMe />
     <Projects />
